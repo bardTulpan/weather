3425e621b6770dcc72cbed830d6eadbbbfd5ec0e
Bard <bard@example.com>
2025-10-27 17:50:14 +0800

    zaglishka mvc

---
+++ src/main/java/bard/wether/controller/AuthController.java
@@ -0,0 +1,26 @@
+package bard.wether.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/auth")
+@RestController
+public class AuthController {
+
+    @PostMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        return "logout";
+    }
+}
---
+++ src/main/java/bard/wether/controller/LocationController.java
@@ -0,0 +1,37 @@
+package bard.wether.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api/locations")
+@RestController
+public class LocationController {
+
+    @GetMapping("/search")
+    public String searchLocation() {
+        return "search";
+    }
+
+    @PostMapping
+    public String save() {
+        return "post location";
+    }
+
+    @GetMapping
+    public String getLocations() {
+        return "getLocations";
+    }
+
+    @DeleteMapping("/{locationId}")
+    public String deleteLocation(@PathVariable Long locationId) {
+        return "Локация с ID " + locationId + " удалена из вашего списка";
+    }
+
+
+
+}
+
+
+//GET    /api/locations/search?q={query}          # Поиск локаций
+//POST   /api/locations                           # Добавление локации в список
+//GET    /api/locations                           # Просмотр списка локаций пользователя
+//DELETE /api/locations/{locationId}              # Удаление локации из списка
